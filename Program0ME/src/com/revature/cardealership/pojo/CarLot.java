9478a4d7d940d2ea2dd0476d15915143323bdf22
mikaelaenters <mikaelaenters@icloud.com>
2019-06-06 09:39:31 -0400

    Removed testing comments

--- Program0ME/src/com/revature/cardealership/pojo/CarLot.java
+++ Program0ME/src/com/revature/cardealership/pojo/CarLot.java
@@ -4,8 +4,7 @@ import java.util.List;
 
 public class CarLot {
 	private List<Car> carLot;
-	//Added for testing.....
-	private int testing= 0;
+
 	public CarLot(List<Car> carLot) {
 		this.carLot = carLot;
 	}
